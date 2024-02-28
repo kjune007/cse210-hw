7832236dd330633ee0f4b933b64a7a081a63b8d8
kjune007 <kjune007@gmail.com>
2024-02-27 19:37:26 -0700

    completed prep assingment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            Console.WriteLine("Good Job! You have passed the class");
+        } 
+        else if (grade >= 80)
+        {
+            letter = "B";
+            Console.WriteLine("Good Job! You have passed the class");
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            Console.WriteLine("Good Job! You have passed the class");
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            Console.WriteLine("You have failed the class. Study harder next time!");
+
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine("You have failed the class. Study harder next time!");
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
     }
 }
