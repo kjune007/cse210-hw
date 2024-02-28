e0198657c394b214acaa09fe53a81f5f948fa9ee
kjune007 <kjune007@gmail.com>
2024-02-28 10:49:29 -0700

    completed the prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int num = rnd.Next(1,100);
+        bool flag = false;
+        while (flag == false)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            int guess = int.Parse(userGuess);
+            if (guess > num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < num)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("That's it!! ");
+                flag = true;
+            }
+
+        }
     }
 }
