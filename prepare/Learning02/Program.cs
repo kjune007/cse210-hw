ed264ac98bf45990bd67048b127e83e316923f83
kjune007 <kjune007@gmail.com>
2024-03-07 17:46:29 -0700

    finised week 3 prepare activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2014;
+        job1._endYear = 2018;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Google";
+        job2._startYear = 2018;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
     }
 }
